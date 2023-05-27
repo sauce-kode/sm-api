e14930ced1e0f68bdaa033fcbd93769297871149
Damilare <dblacktin@gmail.com>
2023-05-27 22:30:30 +0100

    Define post model

---
+++ src/components/post/post.model.ts
@@ -0,0 +1,86 @@
+import { Model, Optional } from "sequelize"
+import {sequelize, DataTypes} from "../../database/postgres"
+import User from "../user/user.model"
+
+interface PostAttributes {
+    id: string,
+    userId: string,
+    title: string,
+    content: string,
+    slug: string,
+    likeCount: number,
+    commentCount: number,
+    created_at?: Date,
+    updated_at?: Date,
+    deleted_at?: Date
+}
+
+export interface PostInput extends Optional<PostAttributes, 'id'> {}
+export interface PostOutput extends Required<PostAttributes> {}
+
+class Post extends Model<PostAttributes, PostInput> implements PostAttributes {
+    public id: string
+    public userId: string
+    public title: string
+    public content: string
+    public slug: string
+    public likeCount: number
+    public commentCount: number
+
+    // timestamp
+    public readonly created_at!: Date
+    public readonly updated_at!: Date
+    public readonly deleted_at!: Date
+}
+
+Post.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.UUID,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    slug: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    likeCount: {
+        type: DataTypes.NUMBER,
+        defaultValue: 0
+    },
+    commentCount: {
+        type: DataTypes.NUMBER,
+        defaultValue: 0
+    }
+}, {
+    sequelize,
+    paranoid: true, //set soft deletes for data
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: 'deleted_at',
+    hooks: {
+        afterCreate: (post, options) => {
+            console.log("POST >>>", post)
+            console.log("OPTIONS >>>>>", options)
+        } 
+    }
+})
+
+Post.belongsTo(User)
+
+export default Post
