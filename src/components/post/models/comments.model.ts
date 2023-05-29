712f38ac550e4323515509e6a0d0cb9ae50bd844
Damilare <dblacktin@gmail.com>
2023-05-29 01:26:07 +0100

    Defines model for post comments

---
+++ src/components/post/models/comments.model.ts
@@ -0,0 +1,66 @@
+import { Model, Optional } from "sequelize"
+import {sequelizeConnection, DataTypes} from "../../../database/postgres"
+import User from "../../user/models/user.model"
+import Post from "./post.model"
+
+interface CommentAttributes {
+    id: string,
+    userId: string,
+    postId: string,
+    comment: string,
+    created_at?: Date,
+    updated_at?: Date,
+}
+
+export interface CommentInput extends Optional<CommentAttributes, 'id'> {}
+export interface CommentOutput extends Required<CommentAttributes> {}
+
+class Comment extends Model<CommentAttributes, CommentInput> implements CommentAttributes {
+    public id: string
+    public userId: string
+    public postId: string
+    public comment: string
+
+    // timestamp
+    public readonly created_at!: Date
+    public readonly updated_at!: Date
+}
+
+Comment.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    postId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: Post,
+            key: 'id'
+        }
+    },
+    comment: {
+        type: DataTypes.TEXT(),
+        allowNull: true
+    },
+}, {
+    sequelize: sequelizeConnection,
+    tableName: "post_comments",
+    paranoid: true, //set soft deletes for data
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+})
+
+Comment.belongsTo(User)
+Comment.belongsTo(Post)
+
+export default Post
